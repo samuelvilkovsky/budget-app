9af7aeeba3cf4e1069fb7f339fe5a1231b7bfd47
samuelvilkovsky <vilkovskysamuel2@gmail.com>
2023-01-26 10:08:05 +0100

    created expenseform added Expense List to the App.js and modified ExpenseItem from Libra to Euro

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Budget from './components/Budget';
-import Remaining from './components/Remaining';
-import Spent from './components/Spent'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import Spent from "./components/Spent";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
   return (
     <div className="container">
       <h1 className="mt-3">My Budget Planner</h1>
       <div className="row mt-3">
+        {/* adding components */}
         <div className="col-sm">
           <Budget />
         </div>
@@ -20,8 +21,15 @@ const App = () => {
           <Spent />
         </div>
       </div>
+      {/* adding Expenses component */}
+      <h3 className="mt-3">Expenses</h3>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <ExpenseList />
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,8 +6,8 @@ const ExpenseItem = (props) => {
     <li className="list-group-item d-flex justify-content-between align-items-center">
       {props.name}
       <div>
-        <span className="badge badge-primary badge-pill mr-3">
-          £{props.cost}
+        <span className="badge badge-primary badge-pill mr-3 text-danger">
+          €{props.cost}
         </span>
         <TiDelete size="1.5em"></TiDelete>
       </div>
