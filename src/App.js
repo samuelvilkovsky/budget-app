cb27463db69f7319f8556261e28cd1b4688519ad
samuelvilkovsky <vilkovskysamuel2@gmail.com>
2023-01-26 09:38:28 +0100

    Added components to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import Spent from './components/Spent'
+
 
 const App = () => {
-  return <p>Hello React!</p>;
+  return (
+    <div className="container">
+      <h1 className="mt-3">My Budget Planner</h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget />
+        </div>
+        <div className="col-sm">
+          <Remaining />
+        </div>
+        <div className="col-sm">
+          <Spent />
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default App;
--- src/components/Spent.js
+++ src/components/Spent.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+// created new component
 const Spent = () => {
     return(
         <div className="alert alert-primary">
-            <span></span>
+            <span>Spent: 1000</span>
         </div>
     );
 };
