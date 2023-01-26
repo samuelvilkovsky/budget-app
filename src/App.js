2fe194f4950b4459bcf9d7176749371ac6c8367e
samuelvilkovsky <vilkovskysamuel2@gmail.com>
2023-01-26 10:23:33 +0100

    Add AddNewExpense to the app.ns

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Budget from "./components/Budget";
 import Remaining from "./components/Remaining";
 import Spent from "./components/Spent";
 import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
 
 const App = () => {
   return (
@@ -26,6 +27,13 @@ const App = () => {
       <div className="row mt-3">
         <div className="col-sm">
           <ExpenseList />
+        </div>
+      </div>
+      {/* adding AddExpenses component */}
+      <h3 className="mt-3">Add Expense</h3>
+      <div className="row mt-3">
+        <div className="row mt-3">
+          <AddExpenseForm /> 
         </div>
       </div>
     </div>
