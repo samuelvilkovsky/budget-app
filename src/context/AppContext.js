b36ffe0eabe45a12a742b0a4b697893b43965db5
samuelvilkovsky <vilkovskysamuel2@gmail.com>
2023-01-26 10:32:11 +0100

    Created AppContext

---
+++ src/context/AppContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const initialState = {
+  budget: 2000,
+  expenses: [
+    { id: 12, name: "shopping", cost: 40 },
+    { id: 13, name: "holiday", cost: 400 },
+    { id: 14, name: "car service", cost: 50 },
+  ],
+};
+
+export const AppContext = createContext();
